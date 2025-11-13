1ac82784c9190f448fe5fafae810348da559b365
mehmetpektass <pektasmehmett1184@gmail.com>
2025-11-13 08:37:28 +0300

    create the function that processes multi heads in parallel

--- main.py
+++ main.py
@@ -76,6 +76,18 @@ class Head(nn.Module):
         return out
     
 
+class MultiHeadAttention():
+    """ multiple heads of self-attention in parallel """
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(head_size*num_heads, n_embd)
+        self.dropout = nn.Dropout(droput)
+    
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.dropout(self.proj(out))
+        return out
 
 
 class LanguageModel(nn.Module):
